ffed45622022f9fe9c8fc104355706e2bdb3cdd0
Tomas <Tomasds15@gmail.com>
2024-10-23 20:08:38 -0300

    tomas

--- Los8Escalones/src/Jugador.java
+++ Los8Escalones/src/Jugador.java
@@ -17,20 +17,39 @@ public class Jugador {
 
     // Constructor, getters y setters
     
+<<<<<<< Updated upstream
     /**
      * @return int return the id
      */
+=======
+    public Jugador(int id, String nombre, String apellido, String correo, int puntaje, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.puntaje = puntaje;
+        this.estado = estado;
+    }
+
+    public Jugador() {
+    }
+
+>>>>>>> Stashed changes
     public int getId() {
         return id;
     }
 
+<<<<<<< Updated upstream
     /**
      * @return String return the nombre
      */
+=======
+>>>>>>> Stashed changes
     public String getNombre() {
         return nombre;
     }
 
+<<<<<<< Updated upstream
     /**
      * @param nombre the nombre to set
      */
@@ -41,10 +60,13 @@ public class Jugador {
     /**
      * @return String return the apellido
      */
+=======
+>>>>>>> Stashed changes
     public String getApellido() {
         return apellido;
     }
 
+<<<<<<< Updated upstream
     /**
      * @param apellido the apellido to set
      */
@@ -55,10 +77,17 @@ public class Jugador {
     /**
      * @return int return the puntaje
      */
+=======
+    public String getCorreo() {
+        return correo;
+    }
+
+>>>>>>> Stashed changes
     public int getPuntaje() {
         return puntaje;
     }
 
+<<<<<<< Updated upstream
     /**
      * @param puntaje the puntaje to set
      */
@@ -69,13 +98,38 @@ public class Jugador {
     /**
      * @return String return the estado
      */
+=======
+>>>>>>> Stashed changes
     public String getEstado() {
         return estado;
     }
 
+<<<<<<< Updated upstream
     /**
      * @param estado the estado to set
      */
+=======
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+>>>>>>> Stashed changes
     public void setEstado(String estado) {
         this.estado = estado;
     }
