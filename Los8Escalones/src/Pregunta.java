379204a6c0f98a1d076072dd0c83666d4e6a69af
Tomas <Tomasds15@gmail.com>
2024-10-23 14:54:35 -0300

    Tomas

--- Los8Escalones/src/Pregunta.java
+++ Los8Escalones/src/Pregunta.java
@@ -10,3 +10,38 @@ public class Pregunta {
 //y de ahi manejar la pregunta de acuerdo a su tipo
 
 }
+//la clase abstracta puede ser algo asi
+/*public abstract class Pregunta { 
+    protected int id;
+    protected String enunciado;
+    protected String categoria;
+    protected String dificultad;
+
+    public Pregunta(int id, String enunciado, String categoria, String dificultad) {
+        this.id = id;
+        this.enunciado = enunciado;
+        this.categoria = categoria;
+        this.dificultad = dificultad;
+    }
+
+    public abstract boolean esCorrecta(String respuesta);
+} 
+    con sus hijas 
+    
+    public class PreguntaMultipleChoice extends Pregunta {
+    private String respuestaCorrecta;
+
+    public PreguntaMultipleChoice(int id, String enunciado, String categoria, String dificultad, String respuestaCorrecta) {
+        super(id, enunciado, categoria, dificultad);
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+    
+    public class PreguntaAproximacion extends Pregunta {
+    private double valorAproximado;
+
+    public PreguntaAproximacion(int id, String enunciado, String categoria, String dificultad, double valorAproximado) {
+        super(id, enunciado, categoria, dificultad);
+        this.valorAproximado = valorAproximado;
+    } etc etc etc
+    */
+
