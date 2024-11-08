a017135e418109a648d5836edcf9dbc2e4e47048
Tomas <Tomasds15@gmail.com>
2024-11-08 16:35:14 -0300

    Tomas

--- 8Escalones/src/Escalon.java
+++ 8Escalones/src/Escalon.java
@@ -2,11 +2,13 @@ public class Escalon {
     private int idEscalon;
     private int numeroEscalon;
     private String estado;
+    private int idTematica;
 
-    public Escalon(int idEscalon, int numeroEscalon, String estado) {
+    public Escalon(int idEscalon, int numeroEscalon, String estado, int idTematica) {
         this.idEscalon = idEscalon;
         this.numeroEscalon = numeroEscalon;
         this.estado = estado;
+        this.idTematica = idTematica;
     }
 
     public int getIdEscalon() {
@@ -19,5 +21,13 @@ public class Escalon {
 
     public String getEstado() {
         return estado;
+    }
+
+    public int getIdTematica() { //tendria que llamar al Dao de Tematica para obtener la tematica
+        return idTematica;
+    }
+
+    public void setIdEscalon(int idEscalon) {
+        this.idEscalon = idEscalon;
     }
 }
