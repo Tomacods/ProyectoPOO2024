741b750b83dbbf71bfc6a5fa04ea139880ba41ac
federxldan <federoldan229@gmail.com>
2024-11-11 17:55:38 -0300

    ----

--- 8Escalones/src/MultipleChoicePregunta.java
+++ 8Escalones/src/MultipleChoicePregunta.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleChoicePregunta extends Pregunta {
-    private List<String> opciones;
+    private ArrayList<Respuesta> opciones;
     private String respuestaCorrecta;
 
-    public MultipleChoicePregunta(int idPregunta, String enunciado, int idTematica, List<String> opciones, String respuestaCorrecta) {
+    public MultipleChoicePregunta(int idPregunta, String enunciado, int idTematica, String respuestaCorrecta) {
         super(idPregunta, enunciado, idTematica, "multiple choice");
-        this.opciones = opciones;
+        this.opciones = new ArrayList<>();
         this.respuestaCorrecta = respuestaCorrecta;
     }
 
@@ -18,17 +19,27 @@ public class MultipleChoicePregunta extends Pregunta {
     public void imprimirOpciones() {
         String[] abc = {"a", "b", "c", "d"};
         int num = 1;
-        for (String op: opciones) {
-            System.out.println(abc[num] + ". " + op);
+        for (Respuesta op: opciones) {
+            System.out.println(abc[num] + ". " + op.getTexto());
             num = num + 1;
         }
     }
 
-    public List<String> getOpciones() {
+    public void guardarOpciones(int idPregunta/* acá iria el coso de la base de datos */) {
+        for (Respuesta rep: bd){
+            if (rep.getIdPregunta() == idPregunta){
+                opciones.add(rep);
+            }
+        }
+    }
+
+    // getters y setters
+
+    public ArrayList<Respuesta> getOpciones() {
         return opciones;
     }
 
-    public void setOpciones(List<String> opciones) {
+    public void setOpciones(ArrayList<Respuesta> opciones) {
         this.opciones = opciones;
     }
 
