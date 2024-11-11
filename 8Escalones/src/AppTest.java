92fc112f22d2a113d3f6b2c3deef5497508fa99f
federxldan <federoldan229@gmail.com>
2024-11-11 19:15:06 -0300

    --

--- 8Escalones/src/AppTest.java
+++ 8Escalones/src/AppTest.java
@@ -4,9 +4,9 @@ public class AppTest {
     
     public static void main(String[] args) throws Exception {
 
-            PreguntaAproximacion pregunta = new PreguntaAproximacion(1, "¿En qué año fue el mundial en Argentina?", "aproximación", "Deportes", new BigDecimal("1978"));
-        Jugador jugador1 = new Jugador(1, "Juan", "Perez", "juan.perez@example.com", 0, "activo");
-        Jugador jugador2 = new Jugador(2, "Maria", "Gomez", "maria.gomez@example.com", 0, "activo");
+            PreguntaAproximacion pregunta = new PreguntaAproximacion(1, "¿En qué año fue el mundial en Argentina?", "Deportes", new BigDecimal("1978"));
+        Jugador jugador1 = new Jugador(1, "Juan Perez", 0, "activo");
+        Jugador jugador2 = new Jugador(2, "Maria Gomez", 0, "activo");
         
         JuegoTest juego = new JuegoTest();
         Jugador ganador = juego.determinarGanadorAproximacion(pregunta, jugador1, "1970", jugador2, "1982");
