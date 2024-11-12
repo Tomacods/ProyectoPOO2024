130ed095febe521f05f21df71f7d6ec186fc66ae
Mora <mor4lina2003@gmail.com>
2024-11-12 16:52:23 -0300

    mc retornar una pregunta

--- pruebas ABM/DAOS/src/DAOs/MultipleChoiceDAO.java
+++ pruebas ABM/DAOS/src/DAOs/MultipleChoiceDAO.java
@@ -26,12 +26,12 @@ public void insertarPreguntaMultipleChoise(MultipleChoicePregunta pregunta, List
         String queryPregunta = "INSERT INTO Pregunta_multiple_choise (Enunciado, ID_Tematica) VALUES (?, ?)";
         
         try (PreparedStatement statementPregunta = connection.prepareStatement(queryPregunta, PreparedStatement.RETURN_GENERATED_KEYS)) {
-            // Insertar la pregunta
+
             statementPregunta.setString(1, pregunta.getEnunciado());
             statementPregunta.setInt(2, pregunta.getIdTematica());
             statementPregunta.executeUpdate();
 
-            // Obtener el ID de la pregunta recién insertada
+            
             try (ResultSet generatedKeys = statementPregunta.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     int idPregunta = generatedKeys.getInt(1);
@@ -87,6 +87,25 @@ public void insertarPreguntaMultipleChoise(MultipleChoicePregunta pregunta, List
 
 
     //QUERYS
+    public MultipleChoicePregunta obtenerPreguntaMC (int id_pregunta) throws SQLException {
+        String query = "SELECT * FROM pregunta_multiple_choise WHERE id_pregunta_mc = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id_pregunta);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new MultipleChoicePregunta(
+                            resultSet.getInt("ID_pregunta_mc"),
+                            resultSet.getString("enunciado"),
+                            resultSet.getInt("id_tematica"),
+                            //ver q hacer lo del la rta corecta q esta en el cosntructor de mc
+                    );
+                }
+            }
+        }catch (SQLException ex){
+            throw new SQLException("Error al obtener pregunta: " + ex.getMessage());
+        }
+        return null;
+    }
     public ArrayList<Object[]> obtenerPreguntasMCPorTematica(int id) { //DEVUELVE LAS PREGUNTAS DE CIERTA TEMATICA (SEGUN EL ID)
         ArrayList<Object[]> preguntas = new ArrayList<>();
         String query = "select p.id_pregunta_mc,  p.enunciado, p.id_tematica from pregunta_multiple_choise p inner join tematica t on p.id_tematica = t.id_tematica where t.id_tematica=?";
