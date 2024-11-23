3eb0744a3792e4f7e37273ae2230a7219ecfdefd
federxldan <federoldan229@gmail.com>
2024-11-23 16:20:04 -0300

    oaaaaaaoaaaaa

---
+++ Strategy Vista/src/MostrarQA.java
@@ -0,0 +1,4 @@
+public interface MostrarQA {
+
+    void mostrarVista();
+}
---
+++ Strategy Vista/src/StrategyMC.java
@@ -0,0 +1,7 @@
+public class StrategyMC implements MostrarQA {
+    
+    @Override
+    public void mostrarVista() {
+        System.out.println("Acá se mostraría la vista de MC.");
+    }
+}
