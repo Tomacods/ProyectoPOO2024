6242786d11de192747c970c527d4c4a519794a0c
Mora <mor4lina2003@gmail.com>
2024-11-23 16:09:52 -0300

    intento hacer el controlador de abmtematica

---
+++ IGU Mariano/IGU_TF_POO/src/controlador/ControladorABMTematica.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import ventanas.ABMPreguntas;
+import ventanas.ABMTematicas;
+
+public class ControladorABMTematica {
+
+private Tematica tematica;
+private ABMTematicas vistaABMTematicas;
+
+public ControladorABMTematica(Tematica tematica, ABMTematicas vistAbmTematicas){
+    this.tematica= tematica;
+    this.vistaABMTematicas= vistAbmTematicas;
+}
+
+
+    
+}
