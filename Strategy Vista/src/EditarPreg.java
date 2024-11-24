3fd5d3cd80fc8e86aa061ded843bd538ebc6b990
federxldan <federoldan229@gmail.com>
2024-11-24 17:43:09 -0300

    mmmmmmm

--- Strategy Vista/src/EditarPreg.java
+++ Strategy Vista/src/EditarPreg.java
@@ -1,12 +1,25 @@
 public class EditarPreg {
     
-    private String tematica;
-    private String tipoPreg;
+    /* private String tematica;
+    private String tipoPreg; */
+    private MostrarVistaQA mostrarVista;
 
     public EditarPreg() {
         this.tematica = "null";
         this.tipoPreg = "null";
+        this.mostrar = new MostrarVistaQA();
     }
 
-    public selecTipo()
+    public selecTipo(String tipoPreg) {
+        if(tipoPreg == "Multiple choice") {
+            mostrarVista = new StrategyMC();
+        } else {
+            if (tipoPreg == "Aproximacion") {
+                mostrarVista = new StrategyAprox();
+            } else {
+                throw new IllegalArgumentException("Tipo ingresado erroneamente.");
+            }
+        }
+        mostrar.mostrarVista();
+    }
 }
