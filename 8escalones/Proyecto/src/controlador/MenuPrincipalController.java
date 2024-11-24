7ef898a04271194c66980e7bb62356b55dd1e7a4
Tomas <Tomasds15@gmail.com>
2024-11-24 17:15:18 -0300

    prueba ABm

---
+++ 8escalones/Proyecto/src/controlador/MenuPrincipalController.java
@@ -0,0 +1,62 @@
+package Controlador;
+
+import vista.MenuPrincipal;
+import vista.SeleccionarJugador;
+import vista.OpcionesGenerales;
+import vista.ABMJugadores;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuPrincipalController {
+    private MenuPrincipal menuPrincipal;
+
+    public MenuPrincipalController(MenuPrincipal menuPrincipal) {
+        this.menuPrincipal = menuPrincipal;
+        this.menuPrincipal.jButtonJugar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrirSeleccionarJugador();
+            }
+        });
+        this.menuPrincipal.jButtonOpciones.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrirOpcionesGenerales();
+            }
+        });
+        this.menuPrincipal.jButtonPerfil.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrirABMJugadores();
+            }
+        });
+        this.menuPrincipal.jButtonSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                salirDelJuego();
+            }
+        });
+    }
+
+    private void abrirSeleccionarJugador() {
+        SeleccionarJugador seleccionarJugador = new SeleccionarJugador();
+        seleccionarJugador.setVisible(true);
+        menuPrincipal.dispose();
+    }
+
+    private void abrirOpcionesGenerales() {
+        OpcionesGenerales opcionesGenerales = new OpcionesGenerales();
+        opcionesGenerales.setVisible(true);
+        menuPrincipal.dispose();
+    }
+
+    private void abrirABMJugadores() {
+        ABMJugadores abmJugadores = new ABMJugadores();
+        abmJugadores.setVisible(true);
+        menuPrincipal.dispose();
+    }
+
+    private void salirDelJuego() {
+        System.exit(0);
+    }
+}
