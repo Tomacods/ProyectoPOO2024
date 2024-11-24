60e41994b6f329ce2f678732433e236f8243e13a
tomaseb <110575303+tomaseb@users.noreply.github.com>
2024-11-24 14:33:25 -0300

    Vista Modo Admin y Controlador
    Pequeños cambios en modo admin y cracion de controlador

---
+++ controladorModoAdmin.java
@@ -0,0 +1,78 @@
+package controlador;
+import ventanas.ModoAdmin;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class controladorModoAdmin implements ActionListener, MouseListener {
+    private ModoAdmin vistaModoAdmin;
+
+    public controladorModoAdmin(){
+        super();
+        this.vistaModoAdmin = new ModoAdmin(this);
+        vistaModoAdmin.setVisible(true);
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(getVistaModoAdmin().getjButtonABMPreguntas())){
+            //new ControladorABMPreguntas();
+            System.out.println("Abre ventana ABM PREGUNTAS"); //PRUEBA
+            getVistaModoAdmin().dispose(); //Cierra la ventana de Modo Admin
+
+        }
+        if(e.getSource().equals(getVistaModoAdmin().getjButtonABMTematicas())){
+            //new ControladorABMTematicas();
+            System.out.println("Abre ventana ABM TEMATICA");
+            getVistaModoAdmin().dispose();
+        }
+        if(e.getSource().equals(getVistaModoAdmin().getjButtonABMJugadores())){
+            //new ControladorABMJugadores();
+            System.out.println("Abre ventana ABM JUGADORES");
+            getVistaModoAdmin().dispose();
+        }
+        if(e.getSource().equals(getVistaModoAdmin().getjButtonBack())){
+            //New ControladorMenuPrincipal();
+            System.out.println("Abre ventana MENU PRINCIPAL");
+            getVistaModoAdmin().dispose();
+        }
+    }
+
+
+    public ModoAdmin getVistaModoAdmin() {
+        return vistaModoAdmin;
+    }
+    
+}
