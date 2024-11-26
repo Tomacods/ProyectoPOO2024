2d3f8f2ed2ec5edea09eab737761de9efedad05c
Mora <mor4lina2003@gmail.com>
2024-11-26 12:53:42 -0300

    a

--- 8Escalones/Proyecto/src/controlador/Main.java
+++ 8Escalones/Proyecto/src/controlador/Main.java
@@ -4,20 +4,19 @@ import java.sql.SQLException;
 
 public class Main {
    public static void main(String[] args) throws SQLException {
-      // ControladorABMTematica con =new ControladorABMTematica();
-      ControladorSeleccionarJugadores sel = new ControladorSeleccionarJugadores();
+      //MORA
+
+      //new ControladorABMTematica();
+      //new ControladorGameplay();
+      //new ControladorABMPreguntas(); 
+
+      //TOMAS
+
       // controladorModoAdmin a = new controladorModoAdmin();
-      // ControladorABMJugadores c = new ControladorABMJugadores();
-      // ventanas.AdminContraseña a = new AdminContraseña(null)
-      // ControladorAdminContraseña c = new ControladorAdminContraseña();
-      //
-
-      // ControladorGameplay c = new ControladorGameplay();
-
-      // ControladorABMPreguntas c = new ControladorABMPreguntas();
-
-      // SeleccionarJugador sele = new SeleccionarJugador();
       // new MenuPrincipalController();
+
+      //MARIANO
+     // new ControladorSeleccionarJugadores();
 
    }
 }
