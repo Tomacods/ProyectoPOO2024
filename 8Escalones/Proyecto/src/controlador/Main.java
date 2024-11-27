9f3408fa4525b0abbcad196252d9d52b5b652767
Tomas <tomasds15@gmail.com>
2024-11-27 04:00:05 -0300

    Update Main.java

--- 8Escalones/Proyecto/src/controlador/Main.java
+++ 8Escalones/Proyecto/src/controlador/Main.java
@@ -1,24 +1,7 @@
 package controlador;
-import java.util.ArrayList;
-import Modelos.Jugador;
 import java.sql.SQLException;
 public class Main {
 public static void main(String[] args) throws SQLException {
-      // ControladorABMTematica con =new ControladorABMTematica();
-      //ControladorSeleccionarJugadores sel = new ControladorSeleccionarJugadores();
-      // controladorModoAdmin a = new controladorModoAdmin();
-      // ControladorABMJugadores c = new ControladorABMJugadores();
-      // ventanas.AdminContraseña a = new AdminContraseña(null)
-      // ControladorAdminContraseña c = new ControladorAdminContraseña();
-      //
-
-      // ControladorGameplay c = new ControladorGameplay(); 
-//
-      // ControladorABMPreguntas c = new ControladorABMPreguntas();
-
-      // SeleccionarJugador sele = new SeleccionarJugador();
       new MenuPrincipalController();
-      //new ControladorGameplayAproximacion();
   }
-
 }
